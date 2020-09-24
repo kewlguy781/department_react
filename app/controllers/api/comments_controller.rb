2e92104a33d1e1be64a0d3d857d35231bd2c70f6
Jon Roberts <kewlguy@gmail.com>
2020-09-23 22:52:19 -0600

    controller done and checked

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,16 +1,49 @@
 class Api::CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
   def index
+    render json: @product.comments
+  end
+
+  def all
+    render json: Comment.all
   end
 
   def show
   end
 
   def create
+    comment = @product.comments.new(comment_params)
+    if (comment.save)
+      render json: comment
+    else
+      render json: comment.errors, status: 422
+    end
   end
 
   def update
+    if (@comment.update(comment_params))
+      render json: @comment
+    else
+      render json: @comment.errors, status: 422
+    end
   end
 
   def destroy
+  comment = @comment.destroy
+  render json: comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
+  def set_comment
+    @comment = @product.comment.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # TODO add route in the starter
+  namespace :api do
+    resources :products do
+      resources :comments
+    end
+    resources :comments
+  end
 end
