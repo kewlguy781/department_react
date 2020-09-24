3cb109e51b8ed544b14cddac9b69c01c56f1f646
Jon Roberts <kewlguy@gmail.com>
2020-09-23 23:01:09 -0600

    fixed route to list all comments. Now I consider the rails end full stable... I think

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # TODO add route in the starter
   namespace :api do
+    get "/comments/all", to: "comments#all"
     resources :products do
       resources :comments
     end
     resources :comments
+    
   end
 end
