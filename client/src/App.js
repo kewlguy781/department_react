71a5b7f28a1ac83d0d901b3f0d271f75fa98d26b
Jon Roberts <kewlguy@gmail.com>
2020-09-23 23:56:52 -0600

    added home (somethings not right)

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import NavBar from "./components/Navbar"
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import {Container} from "semantic-ui-react"
+import Home from "./components/Home"
 
 function App() {
   return (
     <>
     <NavBar />
-    <h1>Welcome to Jon-Mart!</h1>
-    <h5>where you get everything in a json format!</h5>
+    <Container>
+      <Switch>
+      <Home />
+      <Route exact path="/" componet={Home} />
+      {/* <Route exact path="/" componet={Home} />
+      <Route exact path="/" componet={Home} />
+      <Route exact path="/" componet={Home} />
+      <Route exact path="/" componet={Home} />
+       */}
+</Switch>
+
+    </Container>
     </>
       
 
@@ -16,3 +29,17 @@ function App() {
 }
 
 export default App;
+
+//TODO List
+//Create Department Products Page
+  //update router for department
+//Create Department Products Form (New only)
+//Create Comment view (<Comment>)
+//Create Department Products View with comments
+//Create Department Products View with Comment Form (New only)
+//Create Edit for Products
+//Credit Edit for comments
+//Create delete for Products
+//Create delete for comments
+
+//Styling (Thursday!)
---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+return ( 
+    <div>
+    <h1>Welcome to Jon-Mart!</h1>
+    <h5>where you get everything in a json format!</h5>
+    </div>
+    )
+}
+    export default Home;
